e64c3a61da648ca2f7868e019ade057d67c66ba6
sclash <samuelpellikka@gmail.com>
2022-11-02 13:37:12 +0200

    week2

--- week2/controllers/userController.js
+++ week2/controllers/userController.js
@@ -1,13 +1,13 @@
 'use strict';
 // userController
-const {users, getuser} = require('../models/userModel');
+const {users, getUser} = require('../models/userModel');
 
 const user_list_get = (req, res) => {
   res.json(users);
 };
 
 const user_get = (req, res) => {
-  const user = getuser(req.params.id);
+  const user = getUser(req.params.id);
   console.log('käyttäjä', user);
   res.json(user);
 };
