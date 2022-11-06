b75ba72b7ad4c91d3e199d4961fcb948625090d7
sclash <samuelpellikka@gmail.com>
2022-11-06 20:22:23 +0200

    week2

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -1,21 +1,41 @@
 'use strict';
 // catController
-const {cats, getCat} = require('../models/catModel');
+const {getCat, getAllCats, addCat} = require('../models/catModel');
 
-const cat_list_get = (req, res) => {
-  res.json(cats);
+const cat_list_get = async (req, res) => {
+  const kissat = await getAllCats();
+  res.json(kissat);
 };
 
-const cat_get = (req, res) => {
-  const cat = getCat(req.params.id);
-  console.log('kissa', cat);
-  res.json(cat);
+const cat_get = async (req, res) => {
+  const cat = await getCat(req.params.id);
+  if(cat.length > 0) {
+    res.json(cat.pop());
+  } else {
+    res.send('virhe');
+  }
 };
 
-const cat_post = (reg, res) =>{
-  console.log('cat_post', reg.body, reg.file);
-  res.send('Cat post done.');
-}
+const cat_post = async (req, res) => {
+  console.log('cat_post', req.body, req.file);
+  const data = [
+      req.body.name,
+      req.body.birthdate,
+      req.body.weight,
+      req.body.owner,
+      req.file.filename,
+  ];
+
+  const result = await addCat(data);
+  if(result.affectedRows > 0) {
+    res.json({
+      message: 'cat added',
+      cat_id: result.insertId,
+    });
+  } else {
+    res.send('virhe')
+  }
+};
 
 module.exports = {
   cat_list_get,
--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,7 +1,7 @@
 'use strict';
 const express = require('express');
-const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' })
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 const {cat_list_get, cat_get, cat_post} = require('../controllers/catController');
 const router = express.Router();
 
