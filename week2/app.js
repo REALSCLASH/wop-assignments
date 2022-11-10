27290e3f30f984d8d32fc5e407407eb9f991538c
sclash <samuelpellikka@gmail.com>
2022-11-10 11:34:50 +0200

    week3

--- week2/app.js
+++ week2/app.js
@@ -1,28 +1,29 @@
-'use strict';
-const express = require('express');
-const cors = require('cors');
-const catRoute = require('./routes/catRoute');
-const userRoute = require('./routes/userRoute');
-const {httpError} = require('./utils/errors');
+"use strict";
+
+const express = require("express");
+const catRoute = require("./routes/catRoute");
+const userRoute = require("./routes/userRoute");
+const cors = require("cors");
+const { httpError } = require("./utils/errors");
 const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
-app.use(express.static('uploads'));
-
-app.use('/cat', catRoute);
-app.use('/user', userRoute);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("uploads"));
+app.use("/cat", catRoute);
+app.use("/user", userRoute);
 
 app.use((req, res, next) => {
-  const err = httpError('Not found', 404);
+  const err = httpError("Not found", 404);
   next(err);
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).
-      json({message: err.message || 'Internal server error'});
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Internal server error" });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
