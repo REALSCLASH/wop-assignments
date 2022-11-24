ce52c04ae090e72b384d810bc06792702fe12ef2
sclash <samuelpellikka@gmail.com>
2022-11-24 10:43:44 +0200

    exif

--- week2/utils/imageMeta.js
+++ week2/utils/imageMeta.js
@@ -4,24 +4,19 @@ const ExifImage = require('exif').ExifImage;
 const getCoordinates = (imgFile) => { // imgFile = full path to uploaded image
   return new Promise((resolve, reject) => {
     try {
+      const coordinates = [];
       // TODO: Use node-exif to get longitude and latitude from imgFile
-      try {
-        new ExifImage({ image : imgFile }, function (error, exifData) {
-            if (error) {
-                reject(error);
-             } else{
-                coordinates.push(gpsToDesimal(exifData.gps.GPSLongitude, exifData.gps.GPSLongitudeRef));
-                coordinates.push(gpsToDesimal(exifData.gps.GPSLatitude, exifData.gps.GPSLatitudeRef));
-                resolve(coordinates);
-            }
-        });
+      new ExifImage({ image : imgFile }, function (error, exifData) {
+        if (error) {
+          reject(error)
+        } else {
+          coordinates.push(gpsToDecimal(exifData.gps.GPSLongitude, exifData.gps.GPSLongitudeRef));
+          coordinates.push(gpsToDecimal(exifData.gps.GPSLatitude, exifData.gps.GPSLatitudeRef));
+          // coordinates below should be an array of GPS coordinates in decimal format: [longitude, latitude]
+          resolve(coordinates);
+        }
+      });
     } catch (error) {
-        console.log('Error: ' + error.message);
-    }
-      // coordinates below should be an array of GPS coordinates in decimal format: [longitude, latitude]
-     
-    }
-    catch (error) {
       reject(error);
     }
   });
